64e8db4caf1a0658a6bb08c3404719ca0e5fbb93
Irwin Schmitt <irwinschmitt@gmail.com>
2020-12-24 00:22:32 -0300

    Get cousrses info

--- index.js
+++ index.js
@@ -1,5 +1,68 @@
 const puppeteer = require("puppeteer");
 
+async function goToDepartment(page, departmentId) {
+  console.log(departmentId);
+
+  await page.select("[id='formTurma:inputNivel']", "G");
+  await page.select("[id='formTurma:inputDepto']", departmentId);
+  await page.type("[id='formTurma:inputAno']", "2020");
+  await page.select("[id='formTurma:inputPeriodo']", "2");
+
+  await page.click("[value='Buscar']");
+
+  await page.waitForTimeout(3000);
+
+  const courses = await page.evaluate(() => {
+    const linesArray = Array.from(document.querySelectorAll("tr"));
+
+    return linesArray.map(({ className, innerText }) => {
+      if (className === "agrupador") {
+        return {
+          parent: true,
+          id: innerText.split(" ")[1],
+          title: innerText.split(" - ")[1],
+        };
+      } else if (className.includes("linha")) {
+        return {
+          parent: false,
+          id: innerText.split("\t")[0],
+          period: innerText.split("\t")[1],
+          professor: innerText.split("\t")[2].split(" (")[0],
+          timeLoad: innerText.split("\t")[2].split("(").pop().split("h)")[0],
+          hourCode: innerText.split("\t")[3].replace(" \n", ""),
+        };
+      }
+    });
+  });
+
+  const coursesFormatted = [];
+  var index = -1;
+  courses.forEach((element) => {
+    if (!element) return;
+
+    if (element.parent) {
+      index++;
+      coursesFormatted.push({
+        id: element.id,
+        title: element.title,
+        classes: [],
+      });
+    } else {
+      coursesFormatted[index].classes.push({
+        id: element.id,
+        period: element.period,
+        professor: element.professor,
+        timeLoad: element.timeLoad,
+        hourCode: element.hourCode,
+      });
+    }
+  });
+
+  coursesFormatted.forEach((element) => {
+    console.log(element);
+  });
+}
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -15,20 +78,10 @@ const puppeteer = require("puppeteer");
     }))
   );
 
-  departments.forEach((department) => {
-    console.log(department);
-  });
-
-  // Fill form
-  await page.select("[id='formTurma:inputNivel']", "G");
-  await page.select("[id='formTurma:inputDepto']", "673");
-  await page.type("[id='formTurma:inputAno']", "2020");
-  await page.select("[id='formTurma:inputPeriodo']", "2");
-
-  // Click on search button
-  await page.click("[value='Buscar']");
-
-  // Get courses data
+  // for (let i = 1; i < departments.length; i++) {
+  for (let i = 4; i < 5; i++) {
+    await goToDepartment(page, departments[i].value);
+  }
 
   await browser.close();
 })();
