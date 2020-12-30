5d3a9315ae993f175dd994bad9803a86563b97e3
Irwin Schmitt <irwinschmitt@gmail.com>
2020-12-29 23:32:55 -0300

    Save data in JSON

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const puppeteer = require("puppeteer");
+var fs = require("fs");
 
-async function goToDepartment(page, departmentId) {
-  console.log(departmentId);
+function formatHour(textContent) {}
+
+async function goToDepartment(page, department) {
+  console.log(department.id);
+  console.log(department.title);
 
   await page.select("[id='formTurma:inputNivel']", "G");
-  await page.select("[id='formTurma:inputDepto']", departmentId);
+  await page.select("[id='formTurma:inputDepto']", department.id);
   await page.type("[id='formTurma:inputAno']", "2020");
   await page.select("[id='formTurma:inputPeriodo']", "2");
 
@@ -15,7 +19,7 @@ async function goToDepartment(page, departmentId) {
   const courses = await page.evaluate(() => {
     const linesArray = Array.from(document.querySelectorAll("tr"));
 
-    return linesArray.map(({ className, innerText }) => {
+    return linesArray.map(({ className, innerText, textContent }) => {
       if (className === "agrupador") {
         return {
           parent: true,
@@ -23,13 +27,41 @@ async function goToDepartment(page, departmentId) {
           title: innerText.split(" - ")[1],
         };
       } else if (className.includes("linha")) {
+        var hour = textContent
+          .split(
+            "\n\t\t\t\t\t\t\t\t\t\t\t\n\n\t\n\t\t\n\n\t\t\t\t\t\t\t\t\t\t\t"
+          )[1]
+          .split(
+            "  \n\t\t\t\t\t\t\t\t\t\t\t\n\n\n\t\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\t \n\t\t\t\t\t\t\t\t\t"
+          )[0];
+
+        var re = new RegExp("([A-Z]+)", "g");
+        hour = hour.replace(re, " $ $1");
+        var mapObj = {
+          " às ": "–",
+          ":00": "h",
+          ":": "h",
+          "Segunda-feira": "seg",
+          "Terça-feira": "ter",
+          "Quarta-feira": "qua",
+          "Quinta-feira": "qui",
+          "Sexta-feira": "sex",
+          Sábado: "sáb",
+          Domingo: "dom",
+        };
+
+        var re = new RegExp(Object.keys(mapObj).join("|"), "gi");
+
+        hour = hour.replace(re, (matched) => mapObj[matched]);
+        hour = hour.split(" $ ").slice(1).join(", ");
+
         return {
           parent: false,
           id: innerText.split("\t")[0],
           period: innerText.split("\t")[1],
           professor: innerText.split("\t")[2].split(" (")[0],
           timeLoad: innerText.split("\t")[2].split("(").pop().split("h)")[0],
-          hourCode: innerText.split("\t")[3].replace(" \n", ""),
+          hour,
         };
       }
     });
@@ -53,14 +85,12 @@ async function goToDepartment(page, departmentId) {
         period: element.period,
         professor: element.professor,
         timeLoad: element.timeLoad,
-        hourCode: element.hourCode,
+        hour: element.hour,
       });
     }
   });
 
-  coursesFormatted.forEach((element) => {
-    console.log(element);
-  });
+  return coursesFormatted;
 }
 
 (async () => {
@@ -73,15 +103,34 @@ async function goToDepartment(page, departmentId) {
 
   const departments = await page.$$eval(departmentsSelector, (e) =>
     e.map(({ value, label }) => ({
-      value,
-      label,
+      id: value,
+      title: label,
     }))
   );
 
+  const departmentsFormatted = [];
+
   // for (let i = 1; i < departments.length; i++) {
   for (let i = 4; i < 5; i++) {
-    await goToDepartment(page, departments[i].value);
+    // const departmentInfo = await goToDepartment(page, departments[i]);
+
+    departmentsFormatted.push({
+      id: departments[i].id,
+      title: departments[i].title,
+      courses: await goToDepartment(page, departments[i]),
+    });
   }
+
+  // courses.forEach((element) => {
+  //   console.log(element);
+  // });
+
+  const jsonCourses = JSON.stringify(departmentsFormatted);
+  fs.writeFile("data.json", jsonCourses, (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
 
   await browser.close();
 })();
